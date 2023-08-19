3879ccbf52d5ddc6ed07697a53e17b2bb107eddf
Nazarov Yuri <naz4rov96@yandex.ru>
2023-08-19 20:41:14 +0300

    dotenv

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', default='127.0.0.1, localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
